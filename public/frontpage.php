d6b3032a1ec55cfa7687cc69154954ddfd3a4e90
hxtree <matthewheroux@gmail.com>
2022-01-02 18:57:33 -0600

    Fix not loading issue

--- public/frontpage.php
+++ public/frontpage.php
@@ -65,8 +65,8 @@
             </a>
         </div>
     </div>
-</div>
-<div class="card mb-3">
+
+    <div class="card mb-3">
         <div class="card-body">
             <h5 class="card-title">Training Content</h5>
             <p class="card-text">
